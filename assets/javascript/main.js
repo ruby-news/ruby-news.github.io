948f57c9bada9781755e317b4ee4e11693fb7a57
Sourav Goswami <souravgoswami@protonmail.com>
2021-05-12 14:50:41 +0530

    Fix indentation

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function() {
-	for(let i of document.querySelectorAll('pre')) {
-		let html = i.innerHTML
-		i.innerHTML = html.split("\n").map(x => x.trim() + "\n").join('').trim()
-	}
+  for(let i of document.querySelectorAll('pre')) {
+    let html = i.innerHTML
+    i.innerHTML = html.split("\n").map(x => x.trim() + "\n").join('').trim()
+  }
 })
