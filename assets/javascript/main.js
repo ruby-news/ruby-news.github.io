7bd1e55ead146edc4828283ff662d5a9b88e6fd9
Sourav Goswami <souravgoswami@protonmail.com>
2021-05-17 19:09:12 +0530

    Image lazyload animation

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,34 @@
 window.addEventListener('load', function() {
+  /*
+    Trim all pre tags
+  */
   for(let i of document.querySelectorAll('pre')) {
     let html = i.innerHTML
     i.innerHTML = html.split("\n").map(x => x.trim() + "\n").join('').trim()
+  }
+
+  /*
+    Lazy loading animation on images with .lazyload class
+  */
+  const lazyLoads = document.querySelectorAll('.lazyload')
+
+  for(let i in lazyLoads) {
+    let lazyload = lazyLoads[i]
+    if (!lazyload) continue
+
+    let children = lazyload.children
+    if(!children) continue
+
+    let img = children[0]
+    if(!img) continue
+
+    let animObj = children[1]
+    if(!animObj) continue
+
+    animObj.style.animation = 'lazyloadAnim 1s linear alternate infinite'
+
+    img.onload = () => {
+      setTimeout(() => { animObj.remove() }, 3000)
+    }
   }
 })
