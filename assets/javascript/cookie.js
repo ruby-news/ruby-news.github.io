b21cd5fbd149f18208f57548c4eec3cedf25182b
Sourav Goswami <souravgoswami@protonmail.com>
2021-05-24 15:21:35 +0530

    Closed cookie message saved to localStorage

--- assets/javascript/cookie.js
+++ assets/javascript/cookie.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const cookieContainer = document.getElementById('cookieContainer')
+
+	if(localStorage.dismissedCookie == 1) {
+		let parent = cookieContainer.parentNode
+		if(parent) parent.removeChild(cookieContainer)
+	}
+
 	const close = document.getElementById('cookieClose')
 
 	;(() => {
@@ -7,12 +13,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 		close.onclick = () => {
 			cookieContainer.style.animation = 'fade-out-bottom 1s ease forwards'
+			localStorage.dismissedCookie = 1
 
 			setTimeout(() => {
 				let parent = cookieContainer.parentNode
 				if(parent) parent.removeChild(cookieContainer)
 			}, 1500)
-		}
-	})()
+		}	})()
 
 })
